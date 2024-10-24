acb09eddd9ccd4746a532587280f92694d6ad268
phucnhan <phucnhan0406@gmail.com>
2024-10-24 15:08:20 +0700

    python

--- src/python_backend/ai_models.py
+++ src/python_backend/ai_models.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
 
 def calculate_bmr(user):
     if user['gender'] == 'male':
@@ -26,9 +28,9 @@ def create_nutrition_plan(user, tdee):
     else:
         goal_calories = tdee
 
-    protein = user['weight'] * 2.2
-    fat = goal_calories * 0.25 / 9
-    carbs = (goal_calories - (protein * 4) - (fat * 9)) / 4
+    protein = user['weight'] * 2.2  # 2.2g protein per kg body weight
+    fat = goal_calories * 0.25 / 9  # 25% calories from fat
+    carbs = (goal_calories - (protein * 4) - (fat * 9)) / 4  # Remaining calories from carbs
 
     return {
         'calories': goal_calories,
@@ -37,13 +39,25 @@ def create_nutrition_plan(user, tdee):
         'carbs': carbs
     }
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    user = request.json
-    bmr = calculate_bmr(user)
-    tdee = calculate_tdee(user, bmr)
-    plan = create_nutrition_plan(user, tdee)
-    return jsonify(plan)
+def train_linear_regression_model(user_data):
+    inputs = np.array([[user['age'], user['height'], user['weight'], user['activityLevel']] for user in user_data])
+    outputs = np.array([user['calories'] for user in user_data])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    regression = LinearRegression().fit(inputs, outputs)
+    return regression
+
+def train_lstm_model(user_data):
+    inputs = np.array([[user['age'], user['height'], user['weight'], user['activityLevel']] for user in user_data])
+    outputs = np.array([user['calories'] for user in user_data])
+
+    input_tensor = tf.convert_to_tensor(inputs, dtype=tf.float32)
+    output_tensor = tf.convert_to_tensor(outputs, dtype=tf.float32)
+
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape=(inputs.shape[1], 1)))
+    model.add(LSTM(50))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    model.fit(input_tensor, output_tensor, epochs=100)
+    return model

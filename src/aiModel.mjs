d7e9a65b4444acb1bae1638235487a9c76f3c625
phucnhan <phucnhan0406@gmail.com>
2025-02-08 11:21:42 +0700

    update link proxy, api

--- src/aiModel.mjs
+++ src/aiModel.mjs
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function calculateBMR(user) {
     try {
-        const response = await axios.post('https://kl-be-python.onrender.com/calculate', user);
+        const response = await axios.post('http://10.210.243.255:10000/calculate', user);
         return response.data;
     } catch (error) {
         console.error('Error calculating BMR:', error);
